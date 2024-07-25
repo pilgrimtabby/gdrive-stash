35d9c8d0d9ae0e1bbea3d0c7becc3d52a3f47017
pilgrim_tabby <pilgrimtabby@gmail.com>
2024-07-24 23:02:04 -0600

    Add documentation and typing, fix bugs
    Rename variables and function names to conform with style best practices (I think!)
    Fix errors caused by not putting enum values in parenthesis
    Fix errors caused by not accounting for the fact that strings change from $null to "" when passed as parame
    ters into functions
    Separate GetFileLists into two functions: Get-LocalFileList and Get-DriveFileList
    Simplify the logic in Resolve-DriveDirId
    Fix parameter parsing by implementing splatting in several places
    Shorten all comments to 80 char or less, other lines to 100 char or less
    Lots of other stuff

--- gdrive-stash.ps1
+++ gdrive-stash.ps1
@@ -1,155 +1,454 @@
+<#
+.SYNOPSIS
+Copy a directory's files to a directory in Google Drive.
+
+.DESCRIPTION
+An add-on for the gdrive CLI (https://github.com/glotlabs/gdrive).
+Loops through files in a local directory $srcDir (and its subdirectories if
+$recursive is enabled) and puts them directly into a Google Drive directory
+$destDir.
+
+.PARAMETER srcDir
+The locally stored parent directory of the directory to be backed up.
+Ex: C:\foo\bar\ (trailing backslash optional)
+
+.PARAMETER destDir
+The full path OR the Google Drive ID of the target folder. Files inside srcDir
+will be copied directly inside destDir. Using an ID is faster but the full path
+is easier to obtain.
+Paths begin with "\" (forward slashes are converted to backslashes in Resolve-
+DriveDirId). Passing "\" will copy the files in srcDir into Google Drive's root
+directory. If you want to copy files into a folder called "My Backups" in your
+drive's root, you would pass destDir as "\My Backups".
+
+.PARAMETER destDirIsId
+Tells function to treat $dest as a Google Drive ID, not a pathname.
+Default is $false. Alias is "-i".
+
+.PARAMETER makeParents
+When enabled, tells Resolve-DriveDirId to create any directories in $destDir's
+path that don't already exist. If this is disabled and $destDir doesn't exist,
+the script will exit. Alias is "-p".
+
+.PARAMETER recursive
+Tells function to recursively back up subdirectories in $srcDir.
+Default is $false. Alias is "-r".
+
+.EXAMPLE
+Backup-Dir "C:\foo\bar\mystuff" "\"
+Result: All files in "mystuff" are copied into Google Drive's root directory,
+excluding subdirectories.
+
+.EXAMPLE
+Backup-Dir "C:\foo\bar\mystuff" "\mybackups"
+Result: All files in "mystuff", excluding subdirectories, are copied into
+"mybackups", which resides in Google Drive's root directory. If "mybackups"
+doesn't exist, the script will exit.
+
+.EXAMPLE
+Backup-Dir "C:\foo\bar\mystuff" "\mybackups\todays-date" -p
+Result: Result: All files in "mystuff", excluding subdirectories, are copied
+into "mybackups\todays-date", which resides in Google Drive's root directory.
+If either of those directories doesn't exist, they will be created.
+#>
 param (
-    [Parameter(Mandatory=$true)][string]$src,
-    [Parameter(Mandatory=$true)][string]$dest,
-    [switch][Alias("r")]$recursive = $false,
-    [switch][Alias("p")]$parents = $false,
-    [switch][Alias("i")]$dest_is_id = $false
+    [Parameter(Mandatory=$true)][string]$srcDir,
+    [Parameter(Mandatory=$true)][string]$destDir,
+    [switch][Alias("r")]$recursive,
+    [switch][Alias("p")]$makeParents,
+    [switch][Alias("i")]$destDirIsId
 )
 
-enum FileType {
-    DIR
-    FILE
-}
 
-function FileIsDir($src, $filename) {
-    $file_path = $(Get-Item "$src\$filename")
-    if ($file_path.PSIsContainer) {
-        return [FileType]::DIR
+#########################
+#                       #
+# Function Declarations #
+#                       #
+#########################
+
+function Backup-Dir {
+<#
+    .SYNOPSIS
+    Back up a directory (and optionally, its subdirectories) to Google Drive.
+
+    .DESCRIPTION
+    Iterates through all non-directory files, calling Backup-File on each one.
+    If "recursive" is $true, also recursively backs up subdirectories.
+    See Get-Help gdrive-stash for parameter information and examples.
+#>
+    param (
+        [Parameter(Mandatory=$true)][string]$srcDir,
+        [Parameter(Mandatory=$true)][string]$destDir,
+        [switch]$recursive,
+        [switch]$destDirIsId,
+        [switch]$makeParents
+    )
+
+    # Get list of files in $srcDir
+    $srcFiles = $(Get-LocalFileList $srcDir)
+
+    # Get destId
+    if ($destDirIsId) {
+        $destId = $destDir
+    } else {
+        # Use splatting in case $makeParents isn't passed by calling function
+        $params = @{
+            destDir = $destDir
+        }
+        if ($makeParents) {
+            $params.makeParents = $makeParents
+        }
+        $destId = $(Resolve-DriveDirId @params)
     }
-    return [FileType]::FILE
+
+    # Get list of files in $destDir
+    $destFiles = $(Get-DriveFileList $destId)
+    # It's rtyring to make dest files with dest id
+    foreach ($filename in $srcFiles) {
+        $fileType = $(Test-IsDir $srcDir $filename)
+        $fileId, $driveFileCreateTime = $(Get-DriveFileInfo $filename $fileType $destFiles)
+
+        # Either skip or recursively dive into directories, depending on -r
+        if ($fileType -eq ([FileType]::DIR)) {
+            if (!$recursive) { continue }
+
+            if ($fileId -ne "") {
+                $newDestId = $fileId
+            } else {
+                $newDestId = $(gdrive files mkdir --print-only-id --parent $destId $filename)
+            }
+            Backup-Dir "$srcDir\$filename" $newDestId -destDirIsId -recursive
+
+        # Back up all other file types directly
+        } else {
+            Backup-File $srcDir $filename $destId $fileId $driveFileCreateTime
+        }
+    }
 }
 
-function GetDriveDirIdFromPath($dest) {
-    $dest = $dest.Replace("/", "\").TrimStart("\").TrimEnd("\")
-    $path_dirs = $dest.Split("\")
-    $dir_count = $path_dirs.Length
 
-    $cur_dir_id = ""
-    $cur_dir_info = gdrive files list --field-separator ":DELIMITER?" --order-by name --skip-header --full-name
-    $cur_dir_files = $cur_dir_info -split "`n`r"
-    for ($i = 0; $i -le $dir_count; $i++) {
-        if ($i -eq $dir_count) {
-            return $cur_dir_id
+function Get-LocalFileList {
+<#
+    .SYNOPSIS
+    Return list of all files, including subdirectories, inside a directory.
+
+    .PARAMETER srcDir
+    The directory whose files are returned.
+
+    .OUTPUTS
+    [string] with the relative path to each file, separated by newlines.
+#>
+    param (
+        [Parameter(Mandatory=$true)][string]$srcDir
+    )
+
+    try {
+        return $(Get-ChildItem $srcDir -Name)
+    } catch [System.Management.Automation.ItemNotFoundException] {
+        Write-Output "Error: source $srcDir is not a directory"
+        exit
+    }
+}
+
+
+function Get-DriveFileList {
+<#
+    .SYNOPSIS
+    Return array of strings, each string containing info about a Drive file.
+
+    .DESCRIPTION
+    Gets $destDir's contents as a string, and splits the string into an array. 
+
+    Each entry in the array contains the following information, in order:
+    -Google Drive ID
+    -Filename
+    -File type (folder, regular, document, etc.)
+    -File size (for directories, this is blank)
+    -Date and time file was created
+
+    The delimiter ":DELIMITER?" separates discreet pieces of information
+    in each entry in the array. This string is used because it's unlikely to be
+    used in a filename, and it uses a forbidden filename character for both 
+    Windows ("?") and MacOS (":").
+
+    .PARAMETER destDir
+    The directory holding the files that will be listed.
+
+    .OUTPUTS
+    [string[]]: Array of strings, each holding information about a file in
+    the directory at $destId
+#>
+    param (
+        [Parameter(Mandatory=$true)][string]$destId
+    )
+
+    try {
+        $params =
+            "--field-separator", ":DELIMITER?",
+            "--order-by", "name",
+            "--skip-header",
+            "--full-name"
+        if ($destId -ne "") {
+            $params += "--parent", $destId
         }
+        return $($(gdrive files list $params) -split "`n`r")
 
-        $next_dir = $path_dirs[$i]
-        $found_file = $false
-        ForEach ($cur_file in $cur_dir_files) {
-            $cur_file = $cur_file -split ":DELIMITER?", 0, "simplematch"
-            if ($cur_file[1] -ceq $next_dir -and $cur_file[2] -eq "folder") {
-                $found_file = $true
-                $cur_dir_id = $cur_file[0]
-                $cur_dir_info = gdrive files list --parent $cur_dir_id --field-separator ":DELIMITER?" --order-by name --skip-header --full-name
-                $cur_dir_files = $cur_dir_info -split "`n`r"
-                break
+    # Catch any invalid IDs
+    } catch [System.Management.Automation.RemoteException] {
+        Write-Output "Error: directory with ID $destId not found"
+        exit
+    }
+}
+
+
+function Get-DriveFileInfo {
+<#
+    .SYNOPSIS
+    Search a Google Drive dir for a file matching a local file's name and type.
+
+    .DESCRIPTION
+    Make a case-sensitive search in a Google Drive directory for a file with a
+    given filename and type. If a match isn't found, return two "" values.
+    Possible file types are directory and file (see enum FileType). Case
+    sensitivity is used because Google Drive files are case-sensitive.
+
+    .PARAMETER filename
+    The name of the file to search for.
+
+    .PARAMETER fileType
+    The type of the file to search for.
+    Options are [FileType]::DIR and [FileType]::FILE.
+
+    .PARAMETER driveFileList
+    The array of file entries to search through. Each entry holds the following
+    information, in order:
+    -Google Drive ID
+    -Filename
+    -File type (folder, regular, document, etc.)
+    -File size (for directories, this is blank)
+    -Date and time file was created
+
+    If a null value is passed for this parameter, the foreach loop is skipped.
+
+    .OUTPUTS
+    [string]: Google Drive ID of the file. Blank if no match found.
+    [string]: The creation date and time of the Google Drive file. Blank if no
+        match found.
+#>
+    param (
+        [Parameter(Mandatory=$true)][string]$filename,
+        [Parameter(Mandatory=$true)][FileType]$fileType,
+        [string[]]$driveFileList=@()
+    )
+
+    foreach ($line in $driveFileList) {
+        $fileInfo = $($line -split ":DELIMITER?", 0, "simplematch")
+
+        if ($fileInfo[1] -ceq $filename) {
+            if ($fileType -eq ([FileType]::DIR) -and $fileInfo[2] -eq "folder") {
+                return $fileInfo[0], $fileInfo[4]
+
+            } elseif ($fileType -eq ([FileType]::FILE) -and $fileInfo[2] -ne "folder") {
+                return $fileInfo[0], $fileInfo[4]
             }
         }
+    }
+    return "", ""
+}
+    
 
-        if ($found_file) {
-            continue
-        } elseif ($parents) {
-            if ($cur_dir_id -eq "") {
-                $cur_dir_id = $(gdrive files mkdir --print-only-id $path_dirs[$i])
+function Resolve-DriveDirId {
+<#
+    .SYNOPSIS
+    Gets (or creates) the Google Drive ID for a directory.
+
+    .DESCRIPTION
+    Crawls the path $destDir to its last dir and returns its Google Drive ID.
+    If the path doesn't exist and makeParents is enabled, then all missing
+    directories are created.
+
+    .PARAMETER destDir
+    The directory whose ID will be extracted.
+
+    .PARAMETER makeParents
+    When $true, any directories in $destDir's path that don't exist will be
+    created (case-sensitive). If this option is off and $destDir doesn't exist,
+    the script exits.
+
+    .OUTPUTS
+    [string] nextDirId: The Google Drive ID of $destDir.
+#>
+    param (
+        [Parameter(Mandatory=$true)][string]$destDir,
+        [switch]$makeParents
+    )
+    $params =
+        "--field-separator", ":DELIMITER?",
+        "--order-by", "name",
+        "--skip-header",
+        "--full-name"
+    $currFileList = $($(gdrive files list $params) -split "`n`r")
+    $currDirId = ""  # The return value if $destDir is root
+    # Standardize slashes, remove them from ends to simplify splitting
+    $dirsInPath = $destDir.Replace("/", "\").TrimStart("\").TrimEnd("\").Split("\")
+
+    for ($i=0; $i -lt $dirsInPath.Length; $i++) {
+        $nextDir = $dirsInPath[$i]
+        # We only use the first return value ($destId)
+        $nextDirId = $(Get-DriveFileInfo $nextDir ([FileType]::DIR) $currFileList)[0]
+
+        # Directory exists
+        if ($nextDirId -ne "") {
+            if ($i -eq $($dirsInPath.Length - 1)) {
+                break  # Don't need to call gdrive if last dir
+            }
+            $nextFileList = $($(gdrive files list $params --parent $nextDirId) -split "`n`r")
+
+        # Directory doesn't exist
+        } elseif ($makeParents) {
+
+            # Current directory is not root -- make new dir in most recent dir
+            if ($currDirId -ne "") {
+                $nextDirId = $(gdrive files mkdir --parent $currDirId --print-only-id $nextDir)
+            
+            # Current directory is root -- make new directory in root
             } else {
-                $cur_dir_id = $(gdrive files mkdir --parent $cur_dir_id --print-only-id $path_dirs[$i])
+                $nextDirId = $(gdrive files mkdir --print-only-id $nextDir)
             }
-            $cur_dir_info = gdrive files list --parent $cur_dir_id --field-separator ":DELIMITER?" --order-by name --skip-header --full-name
-            $cur_dir_files = $cur_dir_info -split "`n`r"
-            continue
+
+            # File list is empty, since the new directory is empty
+            $nextFileList = @()
+
         } else {
-            Write-Output "Error: $dest is not a directory"
+            Write-Output "Error: destination $destDir is not a directory (case-sensitive)"
             Write-Output "Use option `"-p`" to recursively create parent dirs"
             exit
         }
+
+        $currDirId = $nextDirId
+        $currFileList = $nextFileList
     }
+    return $nextDirId
 }
 
-function GetDriveFileInfo($src, $filename, $drive_files) {
-    $file_type = $(FileIsDir $src $filename)
 
-    ForEach ($drive_line in $drive_files) {
-        $drive_line = $drive_line -split ":DELIMITER?", 0, "simplematch"
-        # Case sensitive, since Google Drive can contain files w/ the same name but different case
-        if ($drive_line[1] -ceq $filename) {
-            if ($file_type -eq [FileType]::DIR -and $drive_line[2] -eq "folder") {
-                return @($drive_line[0], [FileType]::DIR, $drive_line[4])
-            } elseif ($file_type -eq [FileType]::FILE -and $drive_line[2] -ne "folder") {
-                return @($drive_line[0], $file_type, $drive_line[4])
-            }
+function Backup-File {
+<#
+    .SYNOPSIS
+    Upload or update a local file into Google Drive.
+
+    .DESCRIPTION
+    If a file already exists in the directory at $destId, we check if its last
+    write time is more recent than the Drive file's creation time. If so, we
+    know it's different, so we delete the file from Drive and re-upload it.
+
+    Deleting and re-uploading is only marginally slower than simply updating,
+    and it allows us to reset the Drive file's creation date, since that value
+    is set to be the time of upload. This lets us compare that time with the
+    local write time to see if changes have been made.
+
+    If a file doesn't exist in the dir at $destId yet, then upload it.
+
+    .PARAMETER srcDir
+    The parent directory of the file to back up.
+
+    .PARAMETER filename
+    The name of the file to back up, e.g. myfile.txt.
+
+    .PARAMETER destId
+    The Google Drive ID of the folder we are copying files into.
+
+    .PARAMETER fileId
+    The Google Drive ID of the file we are dealing with, if the file already
+    exists in the dir at $destId. If not passed, the default is an empty string.
+
+    .PARAMETER driveFileCreateTime
+    The date and time at which the file in Google Drive was uploaded, if it
+    exists. If not passed, the default is an empty string.
+#>
+    param (
+        [Parameter(Mandatory=$true)][string]$srcDir,
+        [Parameter(Mandatory=$true)][string]$filename,
+        [Parameter(Mandatory=$true)][string]$destId,
+        [string]$fileId="",
+        [string]$driveFileCreateTime=""
+    )
+
+    # Pre-existing file
+    if ($fileId -ne "") {
+        $localWriteTime = (Get-ChildItem "$srcDir\$filename").
+            LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss")
+        if ($localWriteTime -gt $driveFileCreateTime) {
+            gdrive files delete $fileId
+            gdrive files upload --parent $destId "$srcDir\$filename"
         }
-    }
-    return @($null, $file_type, $null)
-}
 
-function GetFileLists($src, $dest, $dest_is_id) {
-    try {
-        $local_files = $(Get-ChildItem $src -Name)
-    } catch [System.Management.Automation.ItemNotFoundException] {
-        Write-Output "Invalid local directory path: " + $src + " not found"
-        exit
-    }
-
-    if (!$dest_is_id) {
-        $dest_id = $(GetDriveDirIdFromPath $dest)
+    # New file
     } else {
-        $dest_id = $dest
+        gdrive files upload --parent $destId "$srcDir\$filename"
     }
+}
 
-    try {
-        if ($dest_id -eq "") {
-            $drive_info = gdrive files list --field-separator ":DELIMITER?" --order-by name --skip-header --full-name
-        } else {
-            $drive_info = gdrive files list --parent $dest_id --field-separator ":DELIMITER?" --order-by name --skip-header --full-name
-        }
-        $drive_files = $drive_info -split "`n`r"
-    } catch [System.Management.Automation.RemoteException] {
-        echo "Invalid Google Drive id: Dir not found"
-        exit
+
+function Test-IsDir {
+<#
+    .SYNOPSIS
+    Categorize a file into one of two types: non-dir file or dir.
+    See enum FileType.
+
+    .PARAMETER srcDir
+    The file's parent directory. Ex: C:\foo\ (trailing backslash not required)
+
+    .PARAMETER filename
+    The file's name, including extension. Ex: my_file.txt
+
+    .OUTPUTS
+    [FileType]::DIR if $srcDr\$filename is a dir, otherwise [FileType]::FILE.
+#>
+    param (
+        [Parameter(Mandatory=$true)][string]$srcDir,
+        [Parameter(Mandatory=$true)]$filename
+    )
+
+    if ($(Get-Item "$srcDir\$filename").PSIsContainer) {
+        return ([FileType]::DIR)
     }
+    return ([FileType]::FILE)
+}
+
+
+enum FileType {
+<#
+    .SYNOPSIS
+    Categorize a file into one of two types: non-dir file or dir.
+#>
+    DIR
+    FILE
+}
     
-    return $local_files, $drive_files, $dest_id
-}
 
-function BackUpFolder($src, $filename, $dest_id, $drive_file_id, $drive_file_create_time) {
-    if (!$recursive) { return }
-
-    if ($null -ne $drive_file_id) {
-        $dest_id = $drive_file_id
-    } else {
-        $dest_id = $(gdrive files mkdir --print-only-id --parent $dest_id $filename)
-    }
-    LoopThroughSrc "$src\$filename" $dest_id $true
-}
-
-function BackUpFile($src, $filename, $dest_id, $drive_file_id, $drive_file_create_time) {
-    if ($null -ne $drive_file_id) {
-        $local_write_time = (Get-ChildItem "$src\$filename").LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss")
-        if ($local_write_time -gt $drive_file_create_time) {
-            $current_datetime = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
-            (Get-Item "$src\$filename").LastWriteTime = $current_datetime
-            gdrive files delete $drive_file_id
-            gdrive files upload --parent $dest_id "$src\$filename"
-        }
-    } else {
-        gdrive files upload --parent $dest_id "$src\$filename"
-    }
-}
-
-function LoopThroughSrc($src, $dest, $dest_is_id) {
-    $local_files, $drive_files, $dest_id = $(GetFileLists $src $dest $dest_is_id)
-
-    ForEach ($filename in $local_files) {
-        $drive_file_id, $file_type, $drive_file_create_time = $(GetDriveFileInfo $src $filename $drive_files)
-        if ($file_type -eq [FileType]::DIR) {
-            BackUpFolder $src $filename $dest_id $drive_file_id $drive_file_create_time
-        } else {
-            BackUpFile $src $filename $dest_id $drive_file_id $drive_file_create_time
-        }
-    }
-}
+##########
+#        #
+# Script #
+#        #
+##########
 
 # Make sure non-terminating exceptions are caught
 $ErrorActionPreference = "Stop"
 
+# Parse parameters
+$params = @{
+    srcDir = $srcDir
+    destDir = $destDir
+}
+if ($recursive) {
+    $params.recursive = $recursive
+}
+if ($makeParents) {
+    $params.makeParents = $makeParents
+}
+if ($destDirIsId) {
+    $params.destDirIsId = $destDirIsId
+}
 
-LoopThroughSrc $src $dest $dest_is_id
+Backup-Dir @params
